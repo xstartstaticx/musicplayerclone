0b9c1be414d369b580c3c4722bba451bfdaf9354
Boi Schrader <boi1992@t-online.de>
2022-11-14 14:55:15 +0100

    more content and bug fixes

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,18 +1,42 @@
 const playlist = [
     {
         id : 1,
-        title : 'Test 01- Test 01',
-        artist : 'Artest',
+        title : 'Bittersweet',
+        artist : 'Jook',
         thumbnail: require('../img/cover1.jpg'),
-        src: require('../mp3/10074.mp3'),
+        src: require('../mp3/Bittersweet.mp3'),
 
     },
     {
         id : 2,
-        title : 'Test 02- Test 02',
-        artist : 'Artest',
+        title : 'Morning-Station',
+        artist : 'Morning Boys',
         thumbnail: require('../img/cover2.jpg'),
-        src: require('../mp3/10074.mp3'),
+        src: require('../mp3/Morning-Station.mp3'),
+
+    },
+    {
+        id : 3,
+        title : 'Mortimer',
+        artist : 'Brad Majors feat. Daniel Reiller',
+        thumbnail: require('../img/Mortimer-cover-image.webp'),
+        src: require('../mp3/Mortimer (feat. Daniel Reiller).mp3'),
+
+    },
+    {
+        id : 4,
+        title : 'Safe From Harm',
+        artist : 'Massive Attack',
+        thumbnail: require('../img/massive attack- safe from harm.jpg'),
+        src: require('../mp3/massive attack- safe from harm.mp3'),
+
+    },
+    {
+        id : 5,
+        title : 'Last Night In Saint Celoni',
+        artist : 'Payfone',
+        thumbnail: require('../img/payfone-last night in saint celoni .jpg'),
+        src: require('../mp3/payfone-last night in saint celoni .mp3'),
 
     },
 ]
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,31 @@
-import React ,{useState,useRef}from 'react';
+import React ,{useState,useRef,useEffect}from 'react';
 import '../assets/css/card.css';
 import playlist from '../assets/data';
 import { timer } from '../utils/timer';
 
-
-
 const Card = ({props :{musicNumber, setMusicNumber, setOpen}}) => {
     
     const [duration,setDuration] = useState(0);
-    const [currentTime,setcurrentTime] = useState(0);
+    const [currentTime,setCurrentTime] = useState(0);
     const [play,setPlay] = useState(false);
+    const [showVolume,setShowVolume] = useState(false);
+    const [volume, setVolume] = useState(50);
 
-const audioRef = useRef()
+
+    const audioRef = useRef()
+
 
     function handleLoadStart(e){
         const src = e.nativeEvent.srcElement.src;
         const audio = new Audio(src);
-        audio.onloadeddata = function(){
+        audio.onloadedmetadata = function(){
             if(audio.readyState > 0){
                 setDuration(audio.duration)
             }
         }
-        console.log(e.nativeEvent.srcElement.src)
+        if(play) {audioRef.current.play(false)};
+
+        //console.log(e.nativeEvent.srcElement.src)
     }
 
     function handlePlayingAudio(){
@@ -36,16 +40,31 @@ const audioRef = useRef()
 
     function handleTimeUpdate () {
         const currentTime = audioRef.current.currentTime;
-        setcurrentTime(currentTime)
+        setCurrentTime(currentTime)
         
         // console.log(currentTime)
     }
+
     function changeCurrentTime(e){
         const currentTime = Number(e.target.value);
         audioRef.current.currentTime = currentTime;
-        setcurrentTime(currentTime)
+        setCurrentTime(currentTime)
         //console.log(e.target.value)
     }
+
+    function handleNextPrev(n){
+        setMusicNumber(value => {
+            if(n > 0)
+            return value + n > playlist.length -1 ? 0 : value + n;
+            return value + n < 0 ? playlist.length -1 : value + n;
+            
+        })
+    }
+
+    useEffect(() =>{
+        audioRef.current.volume = Number(volume) / 100;
+    }, [volume])
+ 
 
   return (
     <div className="card">
@@ -56,10 +75,9 @@ const audioRef = useRef()
             <i className="material-icons" 
                 onClick={() => setOpen(prev => !prev) }>queue_music</i>
         </div>
-    <div className="img">
-          <img src={playlist[musicNumber].thumbnail} alt="" />
-    </div>
-      
+        <div className="img">
+            <img src={playlist[musicNumber].thumbnail} alt="" />
+        </div>
 
         <div className="details">
             <p className="title">{playlist[musicNumber].title}</p>
@@ -68,8 +86,10 @@ const audioRef = useRef()
 
         <div className="progress">
             <input type="range" min={0} max={duration}
-            value={currentTime} onChange={e => changeCurrentTime} />
+            value={currentTime} onChange={e => changeCurrentTime(e)} />
+            
         </div>
+
         <div className="timer">
             <span>{timer(currentTime)}</span>
             <span>{timer(duration)}</span>
@@ -81,31 +101,47 @@ const audioRef = useRef()
             
             <i className="material-icons">repeat</i>
                 
-            <i className="material-icons"id="prev">skip_previous</i>
+            <i className="material-icons"id="prev"
+                  onClick={() => handleNextPrev(-1)}  
+            >skip_previous</i>
             
                 
                 <div className="play" 
                     onClick={handlePlayingAudio}>
-                    
+                  
                     <i className="material-icons">
-                        {play ? 'pause' : 'play_arrow'}
-                    </i>
-                     
+                        {play ? 'pause' : 'play_arrow'} 
+                    </i>                     
                 </div>
                 
-                <i className="material-icons" id="next">skip_next</i>
+            <i className="material-icons" id="next"
+                 onClick={() => handleNextPrev(1)}  
+            >skip_next</i>
             
             {/* Volume*/}
-                <i className="material-icons" >volume_up</i>
-                    <div className="volume"> {/* class volume & show*/}
-                        <i className="material-icons" >volume_up</i>
-                        <input type="range" min={0} max={100} />
-                        <span>50</span>
+                <i className="material-icons" 
+                onClick={() => setShowVolume(prev => !prev)}>{Number(volume) > 0 ? 'volume_up' : 'volume_off'}</i>
+
+
+                    <div className={`volume ${showVolume ? 'show' : ''}`}> 
+                        {/* NO CLUE WHY THIS IS NOT WORKING  */}
+                        <i className="material-icons" >{Number(volume) === 0 ? 'volume_off ' : 'volume_up'}</i>  
+                        
+                        {/* BIG MISTAKE WATCHOUT */} 
+                        
+                        {/* <i className="material-icons" onClick={() => setVolume (v => Number(v) > 0 ? 0 : 100)} >
+                            {Number(volume) > 0 ? 'volume_up' : 'volume_off'}
+                        </i> */}
+                        
+
+                        <input type="range" min={0} max={100} value={volume}
+                        onChange={e => setVolume(e.target.value)}/>
+                        <span>{volume}</span>
                 </div>
             
                
             
-            <audio src={playlist[musicNumber].src} hidden ref={audioRef}
+            <audio src={playlist[musicNumber].src}  ref={audioRef}
             onLoadStart={handleLoadStart} onTimeUpdate={handleTimeUpdate} />
         </div>
     </div>
