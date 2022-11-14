0a04560ccc4c50a5c400a23d6de494567287d794
Boi Schrader <boi1992@t-online.de>
2022-11-14 16:00:23 +0100

    just information

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,22 @@ import playlist from '../assets/data';
 import { timer } from '../utils/timer';
 
 const Card = ({props :{musicNumber, setMusicNumber, setOpen}}) => {
+
     
+
     const [duration,setDuration] = useState(0);
     const [currentTime,setCurrentTime] = useState(0);
     const [play,setPlay] = useState(false);
     const [showVolume,setShowVolume] = useState(false);
     const [volume, setVolume] = useState(50);
 
+/* Refs */
+
 
     const audioRef = useRef()
+
+
+/* Functions / Handlers */
 
 
     function handleLoadStart(e){
@@ -61,48 +68,74 @@ const Card = ({props :{musicNumber, setMusicNumber, setOpen}}) => {
         })
     }
 
+/* useEffects  */
+
     useEffect(() =>{
         audioRef.current.volume = Number(volume) / 100;
     }, [volume])
  
 
+
+/* Card  */
+
+
+
   return (
+
     <div className="card">
         
         <div className="nav">
+
             <i className="material-icons">expand_more</i>
+
             <span>Now Playing {musicNumber + 1} / {playlist.length}</span>
+            
             <i className="material-icons" 
                 onClick={() => setOpen(prev => !prev) }>queue_music</i>
+
         </div>
         <div className="img">
+
             <img src={playlist[musicNumber].thumbnail} alt="" />
+
         </div>
 
+{/* Song Details / Infos */}
+
         <div className="details">
+
             <p className="title">{playlist[musicNumber].title}</p>
             <p className="artist">{playlist[musicNumber].artist}</p>
+
         </div>
 
         <div className="progress">
-            <input type="range" min={0} max={duration}
-            value={currentTime} onChange={e => changeCurrentTime(e)} />
+
+            <input 
+            type="range" 
+            min={0} 
+            max={duration}
+            value={currentTime} 
+            onChange={e => changeCurrentTime(e)} />
             
         </div>
 
         <div className="timer">
+
             <span>{timer(currentTime)}</span>
             <span>{timer(duration)}</span>
+
         </div>
 
-        {/* MENU */}
+{/* MENU */}
 
         <div className="controls">
             
-            <i className="material-icons">repeat</i>
+            <i className="material-icons"
+            >repeat</i>
                 
             <i className="material-icons"id="prev"
-                  onClick={() => handleNextPrev(-1)}  
+                onClick={() => handleNextPrev(-1)}  
             >skip_previous</i>
             
                 
@@ -111,14 +144,15 @@ const Card = ({props :{musicNumber, setMusicNumber, setOpen}}) => {
                   
                     <i className="material-icons">
                         {play ? 'pause' : 'play_arrow'} 
-                    </i>                     
+                    </i> 
+                                        
                 </div>
                 
             <i className="material-icons" id="next"
                  onClick={() => handleNextPrev(1)}  
             >skip_next</i>
             
-            {/* Volume*/}
+{/* Volume*/}
                 <i className="material-icons" 
                 onClick={() => setShowVolume(prev => !prev)}>{Number(volume) > 0 ? 'volume_up' : 'volume_off'}</i>
 
@@ -128,21 +162,30 @@ const Card = ({props :{musicNumber, setMusicNumber, setOpen}}) => {
                         <i className="material-icons" >{Number(volume) === 0 ? 'volume_off ' : 'volume_up'}</i>  
                         
                         {/* BIG MISTAKE WATCHOUT */} 
-                        
+
                         {/* <i className="material-icons" onClick={() => setVolume (v => Number(v) > 0 ? 0 : 100)} >
                             {Number(volume) > 0 ? 'volume_up' : 'volume_off'}
                         </i> */}
                         
 
-                        <input type="range" min={0} max={100} value={volume}
+                        <input 
+                        type="range" 
+                        min={0} 
+                        max={100} 
+                        value={volume}
                         onChange={e => setVolume(e.target.value)}/>
+
                         <span>{volume}</span>
                 </div>
             
                
             
-            <audio src={playlist[musicNumber].src}  ref={audioRef}
-            onLoadStart={handleLoadStart} onTimeUpdate={handleTimeUpdate} />
+            <audio 
+            src={playlist[musicNumber].src}  
+            ref={audioRef}
+            onLoadStart={handleLoadStart} 
+            onTimeUpdate={handleTimeUpdate} />
+
         </div>
     </div>
   )
