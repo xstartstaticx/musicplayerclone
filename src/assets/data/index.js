660166de842f350d2c9b085c4702ffabc74b8a16
Boi Schrader <boi1992@t-online.de>
2022-11-15 11:30:38 +0100

    dizze rascal song changed to actual song

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -80,7 +80,7 @@ const playlist = [
         title : 'I luv U',
         artist : 'Dizzee Rascal',
         thumbnail: require('../img/dizzee.jpg'),
-        src: require('../mp3/Bittersweet.mp3'),
+        src: require('../mp3/I Luv U (Original Version).mp3'),
 
     },
 ]
