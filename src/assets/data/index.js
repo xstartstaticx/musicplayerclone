33eeb51ac11e6bd9c692ccc4748f40854fc8a469
Boi Schrader <boi1992@t-online.de>
2022-11-15 10:48:08 +0100

    rounded up to 10 tracks

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,22 +1,16 @@
 const playlist = [
+    
     {
         id : 1,
-        title : 'Bittersweet',
-        artist : 'Jook',
-        thumbnail: require('../img/cover1.jpg'),
-        src: require('../mp3/Bittersweet.mp3'),
+        title : 'Gabriel - Live Version',
+        artist : 'Roy Davis Jr ft. Peven Everett',
+        thumbnail: require('../img/Gabriel.jpg'),
+        src: require('../mp3/Gabriel (Live Garage Version).mp3'),
 
     },
+   
     {
         id : 2,
-        title : 'Morning-Station',
-        artist : 'Morning Boys',
-        thumbnail: require('../img/cover2.jpg'),
-        src: require('../mp3/Morning-Station.mp3'),
-
-    },
-    {
-        id : 3,
         title : 'Mortimer',
         artist : 'Brad Majors feat. Daniel Reiller',
         thumbnail: require('../img/Mortimer-cover-image.webp'),
@@ -24,7 +18,7 @@ const playlist = [
 
     },
     {
-        id : 4,
+        id : 3,
         title : 'Safe From Harm',
         artist : 'Massive Attack',
         thumbnail: require('../img/massive attack- safe from harm.jpg'),
@@ -32,11 +26,61 @@ const playlist = [
 
     },
     {
+        id : 4,
+        title : 'Loverboy (MJ Cole Remix)',
+        artist : 'Mariah Carey',
+        thumbnail: require('../img/Loverboy.jpg'),
+        src: require('../mp3/Loverboy (MJ Cole Remix).mp3'),
+
+    },
+
+    {
         id : 5,
         title : 'Last Night In Saint Celoni',
         artist : 'Payfone',
         thumbnail: require('../img/payfone-last night in saint celoni .jpg'),
         src: require('../mp3/payfone-last night in saint celoni .mp3'),
+
+    },
+    {
+        id : 6,
+        title : 'Oi! (Original Version',
+        artist : 'More Fire Crew',
+        thumbnail: require('../img/Oi.jpg'),
+        src: require('../mp3/Oi (Original Version).mp3'),
+
+    },
+
+    {
+        id : 7,
+        title : 'RIP Groove (Original Mix).mp3',
+        artist : 'Double 99',
+        thumbnail: require('../img/Rip.jpg'),
+        src: require('../mp3/RIP Groove (Original Mix).mp3'),
+
+    },
+    {
+        id : 8,
+        title : 'Morning-Station',
+        artist : 'Morning Boys',
+        thumbnail: require('../img/cover2.jpg'),
+        src: require('../mp3/Morning-Station.mp3'),
+
+    },
+    {
+        id : 9,
+        title : 'Bittersweet',
+        artist : 'Jook',
+        thumbnail: require('../img/cover1.jpg'),
+        src: require('../mp3/Bittersweet.mp3'),
+
+    },
+    {
+        id : 10,
+        title : 'I luv U',
+        artist : 'Dizzee Rascal',
+        thumbnail: require('../img/dizzee.jpg'),
+        src: require('../mp3/I Luv U (Original Version).mp3'),
 
     },
 ]
