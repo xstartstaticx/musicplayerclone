9f4facfe3b43512c7baebac5220fa2aee0e58e79
xstartstaticx <daniel.reiller@icloud.com>
2024-03-03 23:30:45 +0100

    removed react-scripts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+//  "react-scripts": "5.0.1",
+//  was deleted from package.json because of vulnerability
